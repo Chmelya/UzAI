fcb7144949c8e3b626e9a6599685ab57d18f96d4
Dzmitry <chmel.dmitriy@mail.ru>
2026-02-03 22:17:28 +0300

    Add project references, Swagger, target .NET 9 for build

--- UzAI-backend/UzAI.Api/Program.cs
+++ UzAI-backend/UzAI.Api/Program.cs
@@ -1,12 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "UzAI Api",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "UzAI Api v1");
+    });
 }
 
 app.UseHttpsRedirection();
